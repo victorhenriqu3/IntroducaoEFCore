e8e32accbafcc7fc6a40b559960756441f30e504
Victor Henrique <victorhenriqu3@outlook.com>
2022-03-08 21:47:51 -0400

    Fixing ILogger and displaying operations

--- Order/Data/ApplicationContext.cs
+++ Order/Data/ApplicationContext.cs
@@ -1,15 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using OrderingSystem.Domain;
+using Microsoft.Extensions.Logging;
 namespace OrderingSystem.Data
 {
   public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create((p=>p.AddConsole()));
         DbSet<Pedido> Pedidos { set; get; }
         DbSet<Cliente> Clientes { set; get; }
         DbSet<Produto> Produtos { set; get; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=OrderingSystem");
+            optionsBuilder
+            .UseLoggerFactory(_logger)
+            .EnableSensitiveDataLogging()
+            .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=OrderingSystem");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
